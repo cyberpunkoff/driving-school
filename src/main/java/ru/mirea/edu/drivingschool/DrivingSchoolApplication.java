bb110ba7b18d5cfd6ba166d4eb721569d0b79408
Vasilii Lastin <vasya.lastin76@gmail.com>
2024-05-07 22:33:04 +0300

    chore: initial commit

---
+++ src/main/java/ru/mirea/edu/drivingschool/DrivingSchoolApplication.java
@@ -0,0 +1,11 @@
+package ru.mirea.edu.drivingschool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrivingSchoolApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DrivingSchoolApplication.class, args);
+    }
+}
---
+++ src/test/java/ru/mirea/edu/drivingschool/DrivingSchoolApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.mirea.edu.drivingschool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DrivingSchoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
